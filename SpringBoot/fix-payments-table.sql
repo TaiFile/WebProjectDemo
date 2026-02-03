28b1f93cb2c1f4cd7f88a59944f446beb26f33b3
Vitor Taichi Taira <vitorttaira@gmail.com>
2026-02-03 18:37:10 -0300

    Remove application-dev.properties from version control

--- SpringBoot/fix-payments-table.sql
+++
@@ -1,8 +0,0 @@
--- Script para corrigir a tabela payments
--- Execute este script no pgAdmin ou usando psql
-
--- Dropar a tabela payments (cuidado: isso apaga os dados!)
-DROP TABLE IF EXISTS payments CASCADE;
-
--- O Hibernate irá recriar a tabela corretamente na próxima inicialização
--- com a coluna user_id incluída
